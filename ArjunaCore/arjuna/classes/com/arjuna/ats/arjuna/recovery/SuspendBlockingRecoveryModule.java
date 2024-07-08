538b8dc35882cca291bba5c6a7a54c77fb0e0feb
Manuel Finelli <jfinelli@redhat.com>
2024-07-08 10:29:21 +0100

    Added comment to describe how SuspendBlockingRecoveryModule should work in suspendScan

--- ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/recovery/SuspendBlockingRecoveryModule.java
+++ ArjunaCore/arjuna/classes/com/arjuna/ats/arjuna/recovery/SuspendBlockingRecoveryModule.java
@@ -1,7 +1,21 @@
 package com.arjuna.ats.arjuna.recovery;
 
 /**
- * If a recovery module implements this interface, it can veto the suspension of the Recovery Manager
+ * <p>
+ * If a Recovery Module implements this interface, it can veto the suspension
+ * of the Recovery Manager.
+ * </p>
+ * <p>
+ * Note for the implementer:
+ * While the Recovery Manager is suspending, Recovery Modules
+ * (implementing SuspendBlockingRecoveryModule) that indicate they do not want
+ * to block recovery cannot change their decision to want to block recovery
+ * in subsequent recovery cycles.
+ * In other words, during the suspension of the Recovery Manager, once a
+ * Recovery Module (implementing SuspendBlockingRecoveryModule)
+ * switches from `shouldBlockShutdown() == true` to `shouldBlockShutdown() == false`,
+ * it cannot change its mind.
+ * </p>
  */
 public interface SuspendBlockingRecoveryModule extends RecoveryModule {
 
