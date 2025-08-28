a538f4b46ee727ceaec50ea32f7746bbffeb8fbd
Manuel Finelli <jfinelli@redhat.com>
2025-08-28 11:46:46 +0100

    [JBTM-3997] Narayana release process updated

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -11,6 +11,8 @@ function fatal {
 CURRENT=$(echo ${1} | awk -F '-SNAPSHOT' '{ print $1 }')
 NEXT=$(echo ${2} | awk -F '-SNAPSHOT' '{ print $1 }')
 
+ORG=$3
+
 if [ "$NEXT" == "" ]; then
     echo "usage: $0 <current version> <next version>"
     exit 1
@@ -36,67 +38,51 @@ fi
 echo "Proceeding..."
 
 set -e
-TEMP_WORKING_DIR=~/tmp/narayana/$CURRENT/sources/
+TEMP_WORKING_DIR=~/tmp/narayana/$CURRENT/
 mkdir -p $TEMP_WORKING_DIR
 cd $TEMP_WORKING_DIR || fatal
 
-for REPO in documentation quickstart performance narayana
-do
-    echo ""
-    echo "=== TAGGING AND UPDATING $REPO ==="
-    echo ""
+echo ""
+echo "=== TAGGING AND UPDATING NARAYANA ==="
+echo ""
 
-    if ! [[ "$REPO" =~ 'github.com' ]]; then
-      REPO_TO_CLONE="git@github.com:jbosstm/$REPO.git"
-    else
-      # for repositories in different namespace than Narayana expecting the repo is followed by name of repository to be cloned to
-      REPO_TO_CLONE="$REPO"
-      REPO=${REPO##* }
-    fi
+git clone "git@github.com:$ORG/narayana.git" || fatal
+cd narayana
+git checkout $BRANCH || fatal
 
-    git clone $REPO_TO_CLONE || fatal
-    cd $REPO
-    git checkout $BRANCH || fatal
+if [[ "$(uname)" == "Darwin" ]]; then # MacOS
+  find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "" "s/$CURRENT_SNAPSHOT_VERSION/$CURRENT/g" || fatal
+else
+  find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "s/$CURRENT_SNAPSHOT_VERSION/$CURRENT/g" || fatal
+fi
+set +e
+git status | grep "new\|deleted"
+if [ $? -eq 0 ]; then
+  "Found new files changing to tag - very strange!"
+  exit
+fi
+set -e
+# when there is nothing to commit then do not commit
+toCommit=`git diff | wc -l`
+if [ $toCommit -ge 1 ]; then
+  git commit -am "Updated to $CURRENT" || fatal
+fi
+git tag $CURRENT || fatal
 
-    if [[ "$(uname)" == "Darwin" ]] # MacOS
-    then
-      find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "" "s/$CURRENT_SNAPSHOT_VERSION/$CURRENT/g" || fatal
-    else
-      find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "s/$CURRENT_SNAPSHOT_VERSION/$CURRENT/g" || fatal
-    fi
-    set +e
-    git status | grep "new\|deleted"
-    if [ $? -eq 0 ]
-    then
-      "Found new files changing to tag - very strange!"
-      exit
-    fi
-    set -e
-    # when there is nothing to commit then do not commit
-    toCommit=`git diff | wc -l`
-    if [ $toCommit -ge 1 ]; then
-      git commit -am "Updated to $CURRENT" || fatal
-    fi
-    git tag $CURRENT || fatal
-
-    if [[ "$(uname)" == "Darwin" ]] # MacOS
-    then
-      find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "" "s/$CURRENT/$NEXT/g" || fatal
-    else
-      find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "s/$CURRENT/$NEXT/g" || fatal
-    fi
-    set +e
-    git status | grep "new\|deleted"
-    if [ $? -eq 0 ]
-    then
-      "Found new files changing to new snapshot - very strange!"
-      exit
-    fi
-    set -e
-    toCommit=`git diff | wc -l`
-    if [ $toCommit -ge 1 ]; then
-      git commit -am "Updated to $NEXT" || fatal
-    fi
-    git push origin $BRANCH --tags || fatal
-    cd ..
-done
+if [[ "$(uname)" == "Darwin" ]]; then # MacOS
+  find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "" "s/$CURRENT/$NEXT/g" || fatal
+else
+  find . -name \*.java -o -name \*.xml -o -name \*.template -o -name \*.properties -o -name \*.ent -o -name \INSTALL -o -name \README -o -name pre-release-vars.sh -o -name \*.sh -o -name \*.bat -o -name \*.cxx -o -name \*.c -o -name \*.cpp -o -iname \makefile | grep -v ".svn" | grep -v ".git" | grep -v target | grep -v .idea | xargs sed -i "s/$CURRENT/$NEXT/g" || fatal
+fi
+set +e
+git status | grep "new\|deleted"
+if [ $? -eq 0 ]; then
+  "Found new files changing to new snapshot - very strange!"
+  exit
+fi
+set -e
+toCommit=`git diff | wc -l`
+if [ $toCommit -ge 1 ]; then
+  git commit -am "Updated to $NEXT" || fatal
+fi
+git push origin $BRANCH --tags || fatal
