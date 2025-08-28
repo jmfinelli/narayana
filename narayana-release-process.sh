992adac4b44654ed03ce8dc9eb0660c171c33924
Manuel Finelli <jfinelli@redhat.com>
2025-08-28 11:46:46 +0100

    [JBTM-3997] First iteration

--- narayana-release-process.sh
+++ narayana-release-process.sh
@@ -138,7 +138,7 @@ then
   exit
 fi
 # It is important in the deploy step that if you are deploying to nexus you provide a reference to your settings file as the ./build.sh overrides the default settings file discovery of Maven. Please see https://github.com/jbosstm/narayana/wiki/Narayana-Release-Process for details of the settings.xml requirements
-./build.sh clean deploy -Dmaven.repo.local=${PWD}/localm2repo -DskipTests -gs ~/.m2/settings.xml -Dorson.jar.location=$ORSON_PATH -Prelease,community -DskipNexusStagingDeployMojo=false
+./build.sh clean deploy -Dmaven.repo.local=${PWD}/localm2repo -DskipTests -gs ~/.m2/settings.xml -Dorson.jar.location=$ORSON_PATH -Pcommunity -Drelease -DstagingRelease
 if [[ $? != 0 ]]
 then
   echo 1>&2 Could not deploy narayana to nexus
